25cea1674b118cdb693f3e2dafe938059c0a4b66
julieg18 <julieheartcrochet@gmail.com>
2019-02-16 22:59:33 -0600

    started JSX template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Javascript Calculator</h1>
+        <div id="calculator">
+          <div id="display"></div>
+          <div><button id="clear">C</button></div>
+          <div><button id="divide">÷</button></div>
+          <div><button id="seven">7</button></div>
+          <div><button id="eight">8</button></div>
+          <div><button id="nine">9</button></div>
+          <div><button id="four">4</button></div>
+          <div><button id="five">5</button></div>
+          <div><button id="six">6</button></div>
+          <div><button id="one">1</button></div>
+          <div><button id="two">2</button></div>
+          <div><button id="three">3</button></div>
+          <div><button id="zero">0</button></div>
+          <div><button id="decimal">.</button></div>
+          <div><button id="equals">=</button></div>
+        </div>
       </div>
     );
   }
