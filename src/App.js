794682be87b599e5cb5a8431617a118f8385a08d
julieg18 <julieheartcrochet@gmail.com>
2019-02-19 18:27:03 -0600

    finished JSX prototype

--- src/App.js
+++ src/App.js
@@ -4,26 +4,39 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div id="body">
         <h1>Javascript Calculator</h1>
+        <Calculator/>
+      </div>
+    );
+  }
+}
+
+class Calculator extends React.Component {
+  render() {
+    return (
         <div id="calculator">
           <div id="display"></div>
-          <div><button id="clear">C</button></div>
-          <div><button id="divide">÷</button></div>
-          <div><button id="seven">7</button></div>
-          <div><button id="eight">8</button></div>
-          <div><button id="nine">9</button></div>
-          <div><button id="four">4</button></div>
-          <div><button id="five">5</button></div>
-          <div><button id="six">6</button></div>
-          <div><button id="one">1</button></div>
-          <div><button id="two">2</button></div>
-          <div><button id="three">3</button></div>
-          <div><button id="zero">0</button></div>
-          <div><button id="decimal">.</button></div>
-          <div><button id="equals">=</button></div>
+          <div id="buttons">
+            <button class="long-btns" id="clear" data-action="clear">AC</button>
+            <button class="sq-btns key-operator" id="divide" data-action="divide">÷</button>
+            <button class="sq-btns key-operator" id="multiply" data-action="multiply">X</button>
+            <button class="sq-btns num-btns" id="seven">7</button>
+            <button class="sq-btns num-btns" id="eight">8</button>
+            <button class="sq-btns num-btns" id="nine">9</button>
+            <button class="sq-btns key-operator" id="subtract" data-action="sutract">-</button>
+            <button class="sq-btns num-btns" id="four">4</button>
+            <button class="sq-btns num-btns" id="five">5</button>
+            <button class="sq-btns num-btns" id="six">6</button>
+            <button class="sq-btns key-operator" id="add" data-action="add">+</button>
+            <button class="sq-btns num-btns" id="one">1</button>
+            <button class="sq-btns num-btns" id="two">2</button>
+            <button class="sq-btns num-btns" id="three">3</button>
+            <button class="tall-btns num-btns" id="zero">0</button>
+            <button class="sq-btns" id="decimal" data-action="decimal">.</button>
+            <button class="tall-btns" id="equals key-equal" data-action="calculate">=</button>
+          </div>
         </div>
-      </div>
     );
   }
 }
